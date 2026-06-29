980fa7b802dfddf9416e1dc38612c84ab2f50091
Agustin-Islas <agustinislas44@hotmail.com>
2026-06-29 13:17:13 -0300

    agrega ping para despertar el servidor

--- backend-dialysis-record/src/main/java/com/agustin/backend_dialysis_record/security/SecurityConfig.java
+++ backend-dialysis-record/src/main/java/com/agustin/backend_dialysis_record/security/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         // Endpoints públicos: login, register, etc.
-                        .requestMatchers("/auth/**", "/error", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/auth/**", "/ping", "/error", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
 
                         // Endpoints que requieren permiso especifico
                         .requestMatchers("/api/doctors/**").hasRole("DOCTOR")
